617bfabc5458a594989be474862774f048c841db
marceloassis87 <marceloassis50@gmail.com>
2021-07-18 18:57:56 -0300

    Complementando as operações

--- src/main/kotlin/com/DIO/collection/TesteOperacoes.kt
+++ src/main/kotlin/com/DIO/collection/TesteOperacoes.kt
@@ -11,9 +11,23 @@ fun main() {
     println("MAior salario: ${salarios.maxOrNull()}")
     println("Menor salario: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
+
     // ele aplica a expressão desejada e aplica on array
     // se for true ele aloca em outro array e mostra
     val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
     println("------------")
     salariosMaiorQue2500.forEach(){println(it)}
+    println("------------")
+    //ele conta quantos valores estao naquele range
+    println(salarios.count {it in 2000.0..5000.0})
+    //ele varre a coleção e mostra o valor buscado
+    println("------------")
+    println(salarios.find {it == 2250.0})
+    println(salarios.find {it == 500.0})
+
+    //encontre qualquer que a expressão seja valida
+    println("------------")
+    println(salarios.any {it == 1000.0})
+    println(salarios.any {it == 500.0})
+
 }
