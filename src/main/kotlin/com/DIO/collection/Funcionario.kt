24de09737802e2be100616b2ce575512d5df8772
marceloassis87 <marceloassis50@gmail.com>
2021-07-18 19:19:24 -0300

    Criado arquivo TesteList.kt

---
+++ src/main/kotlin/com/DIO/collection/Funcionario.kt
@@ -0,0 +1,15 @@
+package com.DIO.collection
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+){
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salario: $salario
+        """.trimIndent()
+
+
+}
---
+++ src/main/kotlin/com/DIO/collection/TesteList.kt
@@ -0,0 +1,24 @@
+package com.DIO.collection
+
+fun main() {
+    val joao = Funcionario("joao", 2000.0, "CLT")
+    val pedro = Funcionario("pedro", 1500.0,"PJ")
+    val maria = Funcionario("maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao,pedro,maria)
+
+    funcionarios.forEach(){ println(it)}
+    println("--------------------------------")
+    println(funcionarios.find { it.nome == "maria" })
+
+    println("--------------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("--------------------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+}
+
